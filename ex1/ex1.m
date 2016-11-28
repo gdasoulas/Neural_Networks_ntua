0ab4c6a6c345d467859cdd5bf8bdae05e77e7c3f
George Dasoulas <geodas94@gmail.com>
2016-11-28 15:28:29 +0200

    Full data/code for trainlm

--- ex1/ex1.m
+++ ex1/ex1.m
@@ -49,80 +49,49 @@ TestData = mapstd('apply',TestData,PS);
 TestData = processpca('apply',TestData,PS);
 
 clear PS
-%% Bhma 3
-% mlp neural network
 
-mlp_net= newff(TrainData,TrainDataTargets,[10 15]);
-mlp_net.divideParam.trainRatio=0.8;
-mlp_net.divideParam.valRatio=0.2;
-mlp_net.divideParam.testRatio=0;
-mlp_net.trainParam.epochs=1000;
-
-mlp_net=train(mlp_net,TrainData,TrainDataTargets);
-
-TestDataOutput=sim(mlp_net,TestData);
-[accuracy,precision,recall]=eval_Accuracy_Precision_Recall(TestDataOutput,TestDataTargets);
-
-% good measure for combining precise and recall -> F-score - harmonic mean 
-
-F_score = harmmean([precision recall],2);
-
-%% Bhma 4 
-
-F_score = zeros(6,size(Class,1));
+%% Bhma 3 4 
 
 for i=5:5:30
-    mlp_net= newff(TrainData,TrainDataTargets,i,{},'traingd');
-    mlp_net.divideParam.trainRatio=0.8;
-    mlp_net.divideParam.valRatio=0.2;
-    mlp_net.divideParam.testRatio=0;
-    mlp_net.trainParam.epochs=1000;
+    for j=0:5:30
+        for k=1:10
+            if j==0:
+                layers=[i];
+            else
+                layers=[i j];
+            end
+            mlp_net= newff(TrainData,TrainDataTargets,layers,{},'trainlm');
+            mlp_net.divideParam.trainRatio=0.8;
+            mlp_net.divideParam.valRatio=0.2;
+            mlp_net.divideParam.testRatio=0;
+            mlp_net.trainParam.epochs=1000;
+            mlp_net.trainParam.showWindow=false;
 
-    mlp_net=train(mlp_net,TrainData,TrainDataTargets);
+            mlp_net=train(mlp_net,TrainData,TrainDataTargets);
 
-    TestDataOutput=sim(mlp_net,TestData);
-    [accuracy(i/5),precision,recall]=eval_Accuracy_Precision_Recall(TestDataOutput,TestDataTargets);
+            TestDataOutput=sim(mlp_net,TestData);
+            [acc(k),precision,recall]=eval_Accuracy_Precision_Recall(TestDataOutput,TestDataTargets);
 
-    % good measure for combining precise and recall -> F-score - harmonic mean 
+            % good measure for combining precise and recall -> F-score - harmonic mean 
 
-    F_score(i/5,:) = harmmean([precision recall],2);
-end
-
-F_score = F_score';
-
-scatter([5:5:30],accuracy,'filled','red');
-title('Accuracy according to neurons for one hidden layer'); 
-xlabel('Neurons');
-ylabel('Accuracy');
-clear recall precision i TestDataOutput mlp_net
-
-%% Trying with 2 hidden layers , and neurons in first layer=30
-
-F_score = zeros(6,size(Class,1));
-
-for i=5:5:30
-    for j=5:5:30
-        mlp_net= newff(TrainData,TrainDataTargets,[i j]);
-        mlp_net.divideParam.trainRatio=0.8;
-        mlp_net.divideParam.valRatio=0.2;
-        mlp_net.divideParam.testRatio=0;
-        mlp_net.trainParam.epochs=1000;
-
-        mlp_net=train(mlp_net,TrainData,TrainDataTargets);
-
-        TestDataOutput=sim(mlp_net,TestData);
-        [accuracy(i/5),precision,recall]=eval_Accuracy_Precision_Recall(TestDataOutput,TestDataTargets);
-
-        % good measure for combining precise and recall -> F-score - harmonic mean 
-
-        F_score(i/5,:) = harmmean([precision recall],2);
+            Fsc(k,:) = harmmean([precision recall],2);
+        end
+        accuracy(i/5,j/5+1)=mean(acc)
+        F_score(i/5,j/5+1,:)=mean(Fsc,1);
     end
 end
 
-F_score = F_score';
+clear recall precision i TestDataOutput mlp_net Fsc acc j k
 
-scatter([5:5:30],accuracy,'filled','red');
-title('Accuracy according to neurons for two hidden layer r'); 
-xlabel('Neurons');
+%% Saving mat files
+
+save('F_score.mat','F_score');
+save('accuracy.mat','accuracy');
+
+%%
+
+bar([5:5:30],accuracy);
+title('Accuracy according to neurons for two hidden layers'); 
+xlabel('First layer');
 ylabel('Accuracy');
-clear recall precision i mlp_net
+legend('0','5','10','15','20','25','30');
