21e13e2a450a12de77f46204db46d8b4defc938d
George Dasoulas <geodas94@gmail.com>
2016-12-06 10:17:10 +0200

    Results for transfer function

--- ex1/ex1.m
+++ ex1/ex1.m
@@ -50,7 +50,7 @@ TestData = processpca('apply',TestData,PS);
 
 clear PS
 
-%% Bhma 3 4 
+%% Bhma 3,4,5
 
 for i=5:5:30
     for j=0:5:30
@@ -68,12 +68,23 @@ end
 
 clear recall precision i TestDataOutput mlp_net Fsc acc j k
 
-%% Saving mat files
 
-save('F_score_trainlm_withoutstd.mat','F_score');
-save('accuracy_trainlm_withoutstd.mat','accuracy');
+%% Bhma 6a BEST : TRAINLM -> 2 hidden layers : 20 - 15
 
-%%
+% TransferFcn={'hardlim','tansig','logsig','purelin'};
+TransferFcn={'hardlim'};
+for i=1:size(TransferFcn,2)
+[~,acc,Fscore]=create_NN(TrainData,TrainDataTargets,TestData,TestDataTargets,1,[20 15],...
+    'trainlm','learngdm',TransferFcn{i});
+save(strcat('accuracy_',TransferFcn{i},'.mat'),'acc');
+save(strcat('F_score_',TransferFcn{i},'.mat'),'Fscore');
+end
+
+clear mlp acc Fscore TransferFcn i
+
+
+
+%% PLOTS
 
 bar([5:5:30],acclm.accuracy);
 title('Accuracy according to neurons for two hidden layers'); 
@@ -81,10 +92,3 @@ xlabel('First layer');
 ylabel('Accuracy');
 legend('0','5','10','15','20','25','30');
 
-
-
-%% BEST : TRAINLM -> 2 hidden layers : 20 - 15
-
-[mlp,acc,~]=create_NN(TrainData,TrainDataTargets,TestData,TestDataTargets,10,[20 15],...
-    'trainlm','learngdm','tansig');
-acc
